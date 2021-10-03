041e4741301f6794ca895cae4ecf0ea4f21189fa
paulo <paulohen rique2@protonmail.com>
2021-10-02 21:22:59 -0300

    resolvido exercício número 02.

---
+++ EstruturaSequencial/exe02.py
@@ -0,0 +1,8 @@
+'''
+2.Faça um Programa que peça um número e então mostre a mensagem O número informado foi [número].
+
+
+'''
+
+n1 = input('digite um número.:')
+print(f'o nùmero informado foi {n1}')
