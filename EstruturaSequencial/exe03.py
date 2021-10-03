9876a0c5ec626aadf3ef931c4847229a74feba24
paulo <paulohen rique2@protonmail.com>
2021-10-02 21:29:24 -0300

    resolvido exercício número 03.

---
+++ EstruturaSequencial/exe03.py
@@ -0,0 +1,8 @@
+'''
+3. Faça um Programa que peça dois números e imprima a soma.
+'''
+
+n1 = int(input('digite o número a ser somado: '))
+n2 = int(input('digite o segundo número a ser somado: '))
+soma = n1+n2
+print(f'A soma entre os números {n1} e {n2} é {soma}')
